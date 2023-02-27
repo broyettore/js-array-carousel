c3321482dc642690bc9449163fd351151a07db4f
broyettore <broyettore@gmail.com>
2023-02-27 17:40:55 +0100

    Overall carousel structure created

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,19 @@
 'use strict';
 
+// Creates slider div inside main > .container
+const mainContainer = document.querySelector("main > .container");
+const slider = document.createElement("div");
+slider.classList.add("slider");
+mainContainer.append(slider)
+
+// Created previous button inside slider div
+const btnPrev = document.createElement("button");
+btnPrev.classList.add("prev");
+slider.append(btnPrev);
+
+// Created next button inside slider div
+const btnNext = document.createElement("button");
+btnNext.classList.add("next");
+slider.append(btnNext);
+
+
